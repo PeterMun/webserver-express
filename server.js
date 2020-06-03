b096c64368af25a8eb14daa5ef11c7bb1d1b9229
Peter <pedroanibal_jr@hotmail.com>
2020-06-03 12:18:47 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express();
+
+
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express hbs engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+
+
+app.get('/', (req, res) => {
+    //res.send('Hola Mundo');
+
+    res.render('home', {
+        nombre: 'Peter',
+        anio: new Date().getFullYear()
+    });
+
+
+
+
+});
+
+app.get('/about', (req, res) => {
+    //res.send('Hola Mundo');
+
+    res.render('about', {
+        nombre: 'Peter',
+        anio: new Date().getFullYear()
+    });
+
+
+
+
+});
+
+
+
+app.listen(3000, () => {
+
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+
+})
